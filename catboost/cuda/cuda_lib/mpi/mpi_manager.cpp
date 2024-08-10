b13f87d46fe1f335756e8d91e9720a552356f50a
KelbyProger42 <v.asiliy-dolgushin@yandex.ru>
2024-08-10 20:02:01 +0700

    tst

--- catboost/cuda/cuda_lib/mpi/mpi_manager.cpp
+++ catboost/cuda/cuda_lib/mpi/mpi_manager.cpp
@@ -40,7 +40,7 @@ namespace NCudaLib {
         ncclCommInitRank(&NccclComm, HostCount, NcclId, HostId);
 
 
-        CATBOOST_DEBUG_LOG << "Host count: " << HostCount << " Host id: " << HostId << " Local rank: " << NcclLocalRank << " UPDATED "<< << Endl;
+        CATBOOST_DEBUG_LOG << "Host count: " << HostCount << " Host id: " << HostId << " Local rank: " << NcclLocalRank << " UPDATED " << Endl;
         CommandsBuffer.resize(BufferSize);
         MPI_SAFE_CALL(MPI_Buffer_attach(CommandsBuffer.data(), CommandsBuffer.size()));
 
