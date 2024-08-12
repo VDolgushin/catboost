330ce62a70ea7893c02d2bfdaa2bb6fcfc728b19
KelbyProger42 <v.asiliy-dolgushin@yandex.ru>
2024-08-12 23:21:07 +0700

    tst

--- catboost/cuda/cuda_lib/mpi/mpi_manager.cpp
+++ catboost/cuda/cuda_lib/mpi/mpi_manager.cpp
@@ -264,7 +264,7 @@ namespace NCudaLib {
                                              &writeRequest.Request->Request));
 
                     if(GetTaskTag(deviceId) != 1){
-                        CATBOOST_DEBUG_LOG << "APRICOT ALERT BSEND TAG: " << GetTaskTag(deviceId) << Endl;
+                        CATBOOST_DEBUG_LOG << "APRICOT ALERT ISSEND TAG: " << GetTaskTag(deviceId) << Endl;
                     }
 
                     writeRequest.Request->SetState(TMpiRequest::EState::Running);
